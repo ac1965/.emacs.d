64a09fb61b9d98e17debf60a95c10ebb1b5bbde0
YAMASHITA Takao <tjy1965@gmail.com>
2024-10-12 17:55:58 +0900

    Update

--- init.el
+++ init.el
@@ -45,6 +45,14 @@
 ;; (require 'profiler)
 ;; (profiler-start 'cpu)
 
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "Emacs ready in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract after-init-time before-init-time)))
+                     gcs-done)))
+
 (setq debug-on-error nil)
 (setq gc-cons-threshold most-positive-fixnum)
 
