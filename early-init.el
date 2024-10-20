b18949edfc7a31e838ac5354f32152c51c3903e7
YAMASHITA Takao <tjy1965@gmail.com>
2024-10-20 11:32:00 +0900

    Update

--- early-init.el
+++ early-init.el
@@ -19,6 +19,8 @@
 
 ;;; Code:
 
+(setenv "LIBRARY_PATH" "/usr/local/opt/gcc/lib/gcc/14:/usr/local/opt/libgccjit/lib/gcc/14:/usr/local/opt/gcc/lib/gcc/14/gcc/x86_64-apple-darwin23/14")
+
 (add-hook 'emacs-startup-hook
           (lambda ()
             (message "Emacs ready in %s with %d garbage collections."
@@ -26,40 +28,6 @@
                              (float-time
                               (time-subtract after-init-time before-init-time)))
                      gcs-done)))
-
-(setenv "LIBRARY_PATH" "/usr/local/opt/gcc/lib/gcc/14:/usr/local/opt/libgccjit/lib/gcc/14:/usr/local/opt/gcc/lib/gcc/14/gcc/x86_64-apple-darwin23/14")
-
-(defvar default-file-name-handler-alist file-name-handler-alist)
-
-(defconst my:d
-  (file-name-directory
-   (file-chase-links load-file-name))
-  "The giant turtle on which the world rests.")
-
-(defconst my:d:cache (concat my:d ".cache/"))
-(unless (file-exists-p my:d:cache)
-  (make-directory my:d:cache))
-
-(setq debug-on-error nil
-      package-enable-at-startup nil
-      inhibit-default-init nil
-      byte-compile-warnings '(not cl-functions obsolete)
-      file-name-handler-alist nil
-      gc-cons-threshold most-positive-fixnum
-      gc-cons-percentage 1)
-
-(when (boundp 'load-prefer-newer)
-  (setq load-prefer-newer t))
-
-(when (featurep 'native-compile)
-  (setq native-comp-async-report-warnings-errors nil))
-
-(when (and (fboundp 'startup-redirect-eln-cache)
-           (fboundp 'native-comp-available-p)
-           (native-comp-available-p))
-  (startup-redirect-eln-cache
-   (convert-standard-filename
-    (expand-file-name  "eln-cache/" my:d:cache))))
 
 (defun +gc-after-focus-change ()
   "Run GC when frame loses focus."
@@ -78,32 +46,81 @@
      (when (boundp 'after-focus-change-function)
        (add-function :after after-focus-change-function #'+gc-after-focus-change)))))
 
-;; Found on `https://github.com/miklos1/dotemacs/blob/master/early-init.el'.
-;; Ignore X resources; its settings would be redundant with the other settings
-;; in this file and can conflict with later config (particularly where the
-;; cursor color is concerned).
-(advice-add #'x-apply-session-resources :override #'ignore)
+(defvar default-file-name-handler-alist file-name-handler-alist)
 
-;; GUI elements
-(setq frame-resize-pixelwise t
-      frame-inhibit-implied-resize t
-      frame-title-format '("%b")
+(defconst my:d
+  (file-name-directory
+   (file-chase-links load-file-name))
+  "The giant turtle on which the world rests.")
+
+(defconst my:d:cache (concat my:d ".cache/"))
+(unless (file-exists-p my:d:cache)
+  (make-directory my:d:cache))
+
+(when (boundp 'load-prefer-newer)
+  (setq load-prefer-newer t))
+
+(when (featurep 'native-compile)
+  (setq native-comp-async-report-warnings-errors nil))
+
+(when (and (fboundp 'startup-redirect-eln-cache)
+           (fboundp 'native-comp-available-p)
+           (native-comp-available-p))
+  (startup-redirect-eln-cache
+   (convert-standard-filename
+    (expand-file-name  "eln-cache/" my:d:cache))))
+
+(setq debug-on-error nil
+      package-enable-at-startup nil
+      byte-compile-warnings '(not cl-functions obsolete)
+      file-name-handler-alist nil
+      init-file-debug nil
+      enable-recursive-minibuffers t
       ring-bell-function 'ignore
-      use-dialog-box t ; only for mouse events, which I seldom use
-      use-file-dialog nil
-      use-short-answers t
-      inhibit-splash-screen t
-      inhibit-startup-screen t
-      inhibit-x-resources t
-      inhibit-startup-echo-area-message user-login-name ; read the docstring
-      inhibit-startup-buffer-menu t)
+      global-prettify-symbols-mode t
+      text-quoting-style 'straight
+      use-short-answers)
 
-(menu-bar-mode 1)
-(unless (and (display-graphic-p) (eq system-type 'darwin))
-  (push '(menu-bar-lines . 1) default-frame-alist)
-  (push '(fullscreen . maximized) default-frame-alist))
-(push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars) default-frame-alist)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(inhibit-startup-message t)
+ '(inhibit-startup-echo-area-message t)
+ '(initial-scratch-message nil)
+ '(initial-frame-alist
+   '((left-fringe . 0)
+     (right-fringe   . 0)
+     (internal-border-width . 8)
+     (tool-bar-lines . 0)
+     (fullscreen . maximized)))
+ '(menu-bar-mode t)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil)
+ '(initial-buffer-choice t)
+ '(initial-major-mode 'fundamental-mode)
+ '(frame-inhibit-implied-resize t)
+ '(frame-title-format '("%b"))
+ '(frame-resize-pixelwise t)
+ '(cursor-in-non-selected-windows nil)
+ '(font-lock-maximum-decoration nil)
+ '(font-lock-maximum-size nil)
+ '(x-underline-at-descent-line t)
+ '(window-divider-default-right-width 16)
+ '(window-divider-default-places 'right-only)
+ '(create-lockfiles nil)
+ '(tab-width 4)
+ '(history-length 1000)
+ '(history-delete-duplicates t)
+ '(scroll-preserve-screen-position t)
+ '(scroll-conservatively 100)
+ '(indent-tabs-mode nil)
+ '(truncate-lines t)
+ '(tab-bar-mode t)
+ '(gc-cons-threshold most-positive-fixnum)
+ '(gc-cons-percentage 1))
 
 (provide 'early-init)
 ;;; early-init.el ends here
