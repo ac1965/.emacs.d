a0f21af1b8db606352f40b2d6204929c1b0b817e
YAMASHITA Takao <tjy1965@gmail.com>
2024-12-15 12:07:40 +0900

    refactor(prescient): prescientの設定とキーバインドを更新
    - `prescient-save-file` のカスタム設定を削除し、`prescient-save.el` ファイルを削除
    - Embarkのキーバインドを `s-/`, `s-;` から `s-.`, `s-,` に変更
    - Marginaliaの `marginalia-cycle` キーバインド (`M-A`) を追加

--- prescient-save.el
+++
@@ -1,2 +0,0 @@
-
-(funcall prescient-cache-callback :version '5 :history '#s(hash-table test equal data ("~/" 1 ".emacs.d/" 0)) :frequency '#s(hash-table test equal data ("~/" 0.994009 ".emacs.d/" 0.997)) :serial-number '2)
