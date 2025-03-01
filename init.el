ca3264b45ef72f2072ef855468d8b7cf54ec1fa9
YAMASHITA Takao <tjy1965@gmail.com>
2025-03-01 10:32:25 +0900

    refactor(init.el): Custom自動生成ブロックとコメントの削除
    - `custom-set-variables` および `custom-set-faces` の自動生成ブロックを削除
    - GC設定のインラインコメントを削除

--- init.el
+++ init.el
@@ -43,7 +43,7 @@
 ;; Restore GC settings after Emacs startup
 (add-hook 'emacs-startup-hook
           (lambda ()
-            (setq gc-cons-threshold (* 8 1024 1024)))) ; 8MB に戻す
+            (setq gc-cons-threshold (* 8 1024 1024))))
 
 ;; ---------------------------------------------------------------------------
 ;;; Package Settings
@@ -67,15 +67,3 @@
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
