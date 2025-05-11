d552fe36ff178cf6b853f4121b2c11a20b420267
YAMASHITA Takao <tjy1965@gmail.com>
2025-05-11 16:33:14 +0900

    Update

--- tjy.el
+++ tjy.el
@@ -29,25 +29,30 @@
                       :height (* my:font-size 10))
 
   ;; Define essential directories
-  (defconst my:cloud-directory "~/Documents/"
+  (defconst my:d:cloud "~/Documents/"
     "Directory for cloud-synced documents.")
-  (defconst my:blog-directory (concat my:cloud-directory "devel/repos/mysite/")
+  (defconst my:d:blog (concat my:d:cloud "devel/repos/mysite/")
     "Directory for blog development.")
-  (defconst my:capture-blog-file
-    (expand-file-name "all-posts.org" my:blog-directory)
+  (defconst my:f:capture-blog-file
+    (expand-file-name "all-posts.org" my:d:blog)
     "Path to the blog capture file.")
   (defvar my:excluded-directories '("/Users/ac1965/Library/Accounts"))
 
   ;; Function to ensure directories exist
   (defun ensure-directory (dir)
-    "Ensure that DIR exists."
-    (unless (file-directory-p dir)
-      (warn "Directory does not exist: %s" dir)))
+    "Ensure that DIR exists, except for excluded directories."
+    (message "Checking directory: %s" dir)
+    (unless (member dir my:excluded-directories)
+      (when (file-exists-p dir)
+        (unless (file-directory-p dir)
+          (warn "Path exists but is not a directory: %s" dir)))
+      (unless (file-directory-p dir)
+        (warn "Directory does not exist: %s" dir)))))
 
   ;; Ensure essential directories exist
   (mapc #'ensure-directory
-        (list my:cloud-directory
-              my:blog-directory))
+	(list my:d:cloud
+              my:d:blog))
 
   ;; Add custom elisp directories to `load-path`
   (defconst my:elisp-directory "~/.elisp"
@@ -55,12 +60,9 @@
   (dolist (dir (list (expand-file-name my:elisp-directory)))
     (when (and (stringp dir) (file-directory-p dir))
       (add-to-list 'load-path dir)
-      (normal-top-level-add-subdirs-to-load-path))))
+      (normal-top-level-add-subdirs-to-load-path)))
 
-;; ---------------------------------------------------------------------------
-;;; Logitech MX Ergo S Configuration (macOS)
-(use-package emacs
-  :config
+  ;; Logitech MX Ergo S Configuration (macOS)
   ;; Basic mouse settings
   (setq mouse-wheel-scroll-amount '(1 ((shift) . 5) ((control) . 10)))
   (setq mouse-wheel-progressive-speed nil)  ; disable acceleration
@@ -82,3 +84,4 @@
 
 (provide 'ac1965)
 ;;; ac1965.el ends here
+
