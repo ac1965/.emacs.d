be60a230b768f2536638e54a12e8e9c3a47f1a5b
YAMASHITA Takao <tjy1965@gmail.com>
2025-06-22 17:48:16 +0900

    Update

--- user.el
+++ user.el
@@ -3,7 +3,7 @@
 ;; Copyright (c) 2021-2025 YAMASHITA Takao <ac1965@ty07.net>
 ;; Licensed under the GNU General Public License version 3 or later.
 
-;; $Lastupdate: 2025/05/27 23:59:43 $
+;; $Lastupdate: 2025/06/22 17:45:23 $
 
 ;; Keywords: personal, device configuration
 
