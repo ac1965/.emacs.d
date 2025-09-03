c897a2edbd67679af7c8b2242cb5a384d4ddcd0d
YAMASHITA Takao <tjy1965@gmail.com>
2025-09-03 17:04:51 +0900

    Update

---
+++ elpa/dired-filter-20240629.1857/dired-filter-pkg.el
@@ -0,0 +1,14 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "dired-filter" "20240629.1857"
+  "Ibuffer-like filtering for dired."
+  '((dash              "2.10.0")
+    (dired-hacks-utils "0.0.1")
+    (f                 "0.17.0")
+    (cl-lib            "0.3")
+    (emacs             "24"))
+  :url "https://github.com/Fuco1/dired-hacks"
+  :commit "d1a85901c892ba7ec273995070a43cbbbe5d0b37"
+  :revdesc "d1a85901c892"
+  :keywords '("files")
+  :authors '(("Matúš Goljer" . "matus.goljer@gmail.com"))
+  :maintainers '(("Matúš Goljer" . "matus.goljer@gmail.com")))
