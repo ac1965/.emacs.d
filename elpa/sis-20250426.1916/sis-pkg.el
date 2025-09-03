103d9d775babce7e78a938403f1985e249629442
YAMASHITA Takao <tjy1965@gmail.com>
2025-09-03 17:04:51 +0900

    chore(elpa): Emacs パッケージキャッシュと設定ファイルを追加
    - elpa/ 配下に多数のパッケージ(auctex, magit, org-roam, vertico等)を追加
    - README.org の early-init.el eln キャッシュリダイレクト処理を堅牢化
    - native-comp パス変数の互換性チェックを追加
    - 誤って生成された "]" ファイルを追加(org バッファの書き出し残骸と思われる)

---
+++ elpa/sis-20250426.1916/sis-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "sis" "20250426.1916"
+  "Minimize manual input source (input method) switching."
+  '((emacs "27.1"))
+  :url "https://github.com/laishulu/emacs-smart-input-source"
+  :commit "a438a90209289fd12156285c06ce98e13e2f5834"
+  :revdesc "a438a9020928"
+  :keywords '("convenience"))
